8e30fb6e81f03ad6993959425a900bd86e34bd5c
LordTrindade <54083237+LordTrindade@users.noreply.github.com>
2023-05-10 15:43:59 -0300

    Add files via upload

---
+++ EnumIdosoComorbidades.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  9 21:12:22 2023
+
+@author: filip
+"""
+
+from enum import Enum
+ 
+class IdosoComorbidades(Enum):
+    AVC = 1
+    Hipertensao = 2
+    Diabetes = 3
+    Alzheimer = 4
+    Artrite = 5
+    Parkinson =6
