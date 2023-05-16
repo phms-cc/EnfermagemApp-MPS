919ca1d774e897867ff62ac9a2a5939eee929658
LordTrindade <54083237+LordTrindade@users.noreply.github.com>
2023-05-16 13:59:00 -0300

    Update factory_activity_feeding.py

--- src/controller/factory_activity_feeding.py
+++ src/controller/factory_activity_feeding.py
@@ -9,7 +9,7 @@ from activity_feeding import AtividadeAlimentacao
 from factory_activity import Atividade_Factory
 from activity import Atividade
 
-class AtividadeHidratacao_Factory(Atividade_Factory):
+class AtividadeAlimentacao_Factory(Atividade_Factory):
     def __init__(self):
         pass
     
