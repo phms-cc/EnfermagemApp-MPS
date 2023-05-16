48b61cda636031143c904b7c06cfad46f0cfb43c
LordTrindade <54083237+LordTrindade@users.noreply.github.com>
2023-05-16 20:12:00 -0300

    Create user_intra_interface_factory.py

---
+++ src/interface/user_intra_interface_factory.py
@@ -0,0 +1,9 @@
+from user_intra_interface import UserIntraInterface
+
+class UserIntraInterface_Factory:
+	def __init__ (self):
+		pass
+
+	def getUserIntraInter(self):
+		user_intra_interface = UserIntraInterface()
+		return user_intra_interface
