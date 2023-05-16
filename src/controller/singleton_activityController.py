89843e28038d6fc1062fdec7482579111b429cdb
LordTrindade <54083237+LordTrindade@users.noreply.github.com>
2023-05-16 14:21:25 -0300

    Update singleton_activityController.py

--- src/controller/singleton_activityController.py
+++ src/controller/singleton_activityController.py
@@ -54,6 +54,6 @@ class AtividadeController(metaclass=SingletonMeta):
         atividades = paciente.get_atividades()
         print ("Atividades do Paciente " + paciente.getNome())
         for atividade in atividades:
-            print ("Atividade do tipo " + atividade.get_tipo() + " Data: " + atividade.get_data())
+            print ("Atividade do tipo " + atividade.get_tipo() + " Data: " + str(atividade.get_data()))
             
         
