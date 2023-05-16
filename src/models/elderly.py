c0626c75b132491e0a07bd1d82f796d604743253
Mateus <mateus_freitascorreia@hotmail.com>
2023-05-16 10:06:46 -0300

    Correção

--- src/models/elderly.py
+++ src/models/elderly.py
@@ -1,6 +1,5 @@
 from pacient import Paciente
 
 class Idoso(Paciente):
-    def __init__(self, nome, idade, peso, altura, endereço, alguma_propriedade_adicional):
+    def __init__(self, nome, idade, peso, altura, endereço):
         super().__init__(nome, idade, peso, altura, endereço)
-        self.alguma_propriedade_adicional = alguma_propriedade_adicional
