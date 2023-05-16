29148460c02d02a84deba4a1a4454d536829ebaa
LordTrindade <54083237+LordTrindade@users.noreply.github.com>
2023-05-16 13:59:14 -0300

    Update factory_activity_hygiene.py

--- src/controller/factory_activity_hygiene.py
+++ src/controller/factory_activity_hygiene.py
@@ -9,7 +9,7 @@ from activity_hygiene import AtividadeHigiene
 from factory_activity import Atividade_Factory
 from activity import Atividade
 
-class AtividadeHidratacao_Factory(Atividade_Factory):
+class AtividadeHigiene_Factory(Atividade_Factory):
     def __init__(self):
         pass
     
