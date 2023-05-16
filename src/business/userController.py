5d3b87bf509fe280cd71b4861f5acdb1cb35a591
LordTrindade <54083237+LordTrindade@users.noreply.github.com>
2023-05-16 12:32:41 -0300

    Update userController.py

--- src/business/userController.py
+++ src/business/userController.py
@@ -20,3 +20,51 @@ class UserController:
         salvar_user = UserFacade()
         salvar_user.cadastrar_usuario(novo_usuario)
         return novo_usuario
+        
+    def get_user(self,login):
+        salvar_user = UserFacade()
+        user = salvar_user.obter_usuario(login)
+        return user
+        
+    def delete_usuario(self,login):
+        pass
+
+    def change_user_password(self,login,new_password):
+        user = self.get_user(login)
+        user.set_password(new_password)
+                
+    def used_login(self,login):
+        if (self.get_user(login) == None):
+            return False
+        return True
+    
+    def user_get_password(self,login):
+        user = self.get_user(login)
+        return user.get_password()
+
+    def user_compare_password(self,user, password):
+        return ((self.user_get_password())== (password))
+
+    def user_get_email(self,login):
+        user = self.get_user(login)
+        return user.get_email()
+
+    def user_set_email(self,login,email):
+        user = self.get_user(login)
+        user.set_email(email)
+
+    def user_get_telefone(self,login):
+        user = self.get_user(login)
+        return user.get_telefone()
+
+    def user_set_telefone(self,login,telefone):
+        user = self.get_user(login)
+        user.set_telefone(telefone)
+
+    def user_get_nome(self,login):
+        user = self.get_user(login)
+        return user.get_nome()
+
+    def user_set_nome(self,login,nome):
+        user = self.get_user(login)
+        user.set_nome(nome)
