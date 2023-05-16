c53464563cfe3c7b8c952f84eedc092684c48689
LordTrindade <54083237+LordTrindade@users.noreply.github.com>
2023-05-16 10:23:43 -0300

    Update user.py

--- src/models/user.py
+++ src/models/user.py
@@ -3,6 +3,8 @@ class User:
         self.login = login
         self.senha = senha
         self.nome = nome
+         self.email = None
+        self.telefone = None
         self.pacientes = []
 
     def adicionar_paciente(self, paciente):
@@ -10,3 +12,33 @@ class User:
 
     def remover_paciente(self, paciente):
         self.pacientes.remove(paciente)
+        
+    def get_password(self):
+        return self.password
+    
+    def get_login(self):
+        return self.login
+   
+    def set_login(self,new_login):
+        self.login = new_login
+    
+    def set_password(self,new_password):
+        self.password = new_password
+    
+    def get_email(self):
+        return self.email
+    
+    def set_email(self,email):
+        self.email = email
+    
+    def get_telefone(self):
+        return self.telefone
+    
+    def set_telefone(self,telefone):
+        self.telefone = telefone
+    
+    def get_nome(self):
+        return self.nome
+    
+    def set_nome(self,nome):
+        self.nome = nome
