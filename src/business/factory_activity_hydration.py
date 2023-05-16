c1c58741b059823551cce66077bfa9fc5e2afa36
LordTrindade <54083237+LordTrindade@users.noreply.github.com>
2023-05-16 12:40:03 -0300

    Update and rename Factory_AtividadeHidratacao.py to factory_activity_hydration.py

--- src/business/factory_activity_hydration.py
+++ src/business/factory_activity_hydration.py
@@ -4,9 +4,9 @@ Created on Tue May  9 22:04:23 2023
 
 @author: filip
 """
-from AtividadeHidratacao import AtividadeHidratacao
-from AtividadeFactory import Atividade_Factory
-from Atividade import Atividade
+from activity_hydration import AtividadeHidratacao
+from factory_activity import Atividade_Factory
+from activity import Atividade
 
 class AtividadeHidratacao_Factory(Atividade_Factory):
     def __init__(self):
