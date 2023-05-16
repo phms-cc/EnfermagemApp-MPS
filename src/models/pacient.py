e521ee66c8a88b1e711b2f3194dc52b6bdf2af11
LordTrindade <54083237+LordTrindade@users.noreply.github.com>
2023-05-16 10:19:33 -0300

    Update pacient.py

--- src/models/pacient.py
+++ src/models/pacient.py
@@ -8,3 +8,73 @@ class Paciente(ABC):
         self.peso = peso
         self.altura = altura
         self.endereço = endereço
+    
+    @abstractmethod
+    def getID(self):
+        pass
+    
+    @abstractmethod
+    def compareID(self, i):
+       pass
+    
+    @abstractmethod
+    def getNome(self):
+        pass
+    
+    @abstractmethod
+    def setNome(self,nome):
+        pass
+    
+    @abstractmethod
+    def getIdade (self):
+        pass
+    
+    @abstractmethod
+    def setIdade(self,idade):
+        pass
+    
+    @abstractmethod
+    def getPeso(self):
+        pass
+    
+    @abstractmethod
+    def setPeso(self,peso):
+        pass
+    
+    @abstractmethod
+    def getAltura(self):
+        pass
+    
+    @abstractmethod
+    def setAltura(self):
+        pass
+    
+    @abstractmethod
+    def getEndereco(self):
+        pass
+    
+    @abstractmethod
+    def setEndereco(self):
+        pass
+    
+    @abstractmethod
+    def getComorbidades(self):
+        pass
+    
+    @abstractmethod
+    def addComorbidade(self):
+        pass
+    
+    @abstractmethod
+    def hasComorbidade(self):
+        pass
+    
+    @abstractmethod
+    def nComorbidades(self):
+        pass
+    
+    @abstractmethod
+    def removeComorbidade(self,comorbidade):
+        pass
+
+   
