5cebb5b2b9aebe06fe58e496284fbbc63a20e521
LordTrindade <54083237+LordTrindade@users.noreply.github.com>
2023-05-18 14:42:27 -0300

    Create show_image.py

---
+++ src/models/show_image.py
@@ -0,0 +1,8 @@
+class MostraImagem():
+	def __init__(self):
+		pass
+	
+	def mostra_imagem(self,imagem, posicao, altura, largura):
+		pass
+		#funcao de mostrar imagem, seja como for
+		#ex imagem.mostra_imagem(posicao, altura, largura)
