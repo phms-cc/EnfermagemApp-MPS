1e39dbc1b589f8eee8512613f33f655c9d630cf8
LordTrindade <54083237+LordTrindade@users.noreply.github.com>
2023-05-18 10:56:06 -0300

    Update user_intra_interface_factory.py

--- src/interface/user_intra_interface_factory.py
+++ src/interface/user_intra_interface_factory.py
@@ -1,9 +1,15 @@
 from user_intra_interface import UserIntraInterface
+from user_intra_interface_abstractfactory import UserIntraInterface_AbstractFactory
 
-class UserIntraInterface_Factory:
+
+class UserIntraInterface_Factory(UserIntraInterface_AbstractFactory):
 	def __init__ (self):
 		pass
 
 	def getUserIntraInter(self):
 		user_intra_interface = UserIntraInterface()
 		return user_intra_interface
+
+	def cadastrar_usuario(self,usuario):
+		produto = self.getUserIntraInter()
+		produto.cadastrar_usuario(usuario)
