1567753de153fd254e0b5a166ad067ae7f45f3a0
guilherme.ns <guilherme.silva.ex@synchro.com.br>
2023-05-30 16:02:09 -0300

    adicionando métodos do paciente na fachada

--- src/controller/patientController.py
+++ src/controller/patientController.py
@@ -6,49 +6,49 @@ class PacienteController:
         novo_paciente = Idoso(nome, idade, peso, altura, endereço)
         
         return novo_paciente
-   
-    def list_comorbidades(self,paciente):
-        comorbidades = paciente.getComorbidades()
-        for comorbidade in comorbidades:
-            print(comorbidade, end = " ")
-
-    def list_paciente(self,paciente):
-        print("Nome: " + paciente.getNome() + " idade:" + str(paciente.getIdade()),end = "")
-        print(" peso:" + str(paciente.getPeso())+ " altura:" + str(paciente.getAltura()))
-        
+    
     def list_pacientes(self,pacientes):
         for paciente in pacientes:
-            self.list_paciente(paciente)
+            print("Nome: " + paciente.getNome() + " idade:" + str(paciente.getIdade()),end = "")
+            print(" peso:" + str(paciente.getPeso())+ " altura:" + str(paciente.getAltura()))
             
-    def delete_paciente(self,i):
+    def delete_paciente(self):
         pass             
             
-    def paciente_getNome(self,paciente):
-        paciente.getNome()
+    def get_nome(self,paciente):
+        return paciente.getNome()
 
-    def paciente_setNome(self,paciente,nome):
+    def set_nome(self,paciente,nome):
         paciente.setNome(nome)
 
-    def paciente_getIdade(self,paciente):
-        paciente.getIdade()
+    def get_idade(self,paciente):
+        return paciente.getIdade()
 
-    def paciente_setIdade(self,paciente,idade):
+    def set_idade(self,paciente,idade):
         paciente.setIdade(idade)
 
-    def paciente_getAltura(self,paciente):
-        paciente.getAltura()
+    def get_altura(self,paciente):
+        return paciente.getAltura()
 
-    def paciente_setAltura(self,paciente,altura):
+    def set_altura(self,paciente,altura):
         paciente.setAltura(altura)
 
-    def paciente_getPeso(self,paciente):
-        paciente.getPeso()
+    def get_peso(self,paciente):
+        return paciente.getPeso()
 
-    def paciente_setPeso(self,paciente,peso):
+    def set_peso(self,paciente,peso):
         paciente.setPeso(peso)
     
-    def paciente_getEndereco(self,paciente):
-        paciente.getEndereco()
+    def get_endereco(self,paciente):
+        return paciente.getEndereco()
 
-    def paciente_setEndereco(self,paciente,endereco):
+    def set_endereco(self,paciente,endereco):
         paciente.setPeso(endereco)
+
+    def get_comorbidades(self, paciente):
+        return paciente.getComorbidades()
+    
+    def list_comorbidades(self,paciente):
+        comorbidades = get_comorbidades(paciente)
+        for comorbidade in comorbidades:
+            print(comorbidade, end = " ")
