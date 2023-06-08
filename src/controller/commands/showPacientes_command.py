cbd328dcff9860962398965fd81b8a6a9cebf5d8
Pedro Marinho <81970487+phms-cc@users.noreply.github.com>
2023-06-08 17:02:11 -0300

    Corrigindo um command do Padrão Command

--- src/controller/commands/showPacientes_command.py
+++ src/controller/commands/showPacientes_command.py
@@ -6,9 +6,5 @@ class showPacientes(CommandInterface):
         self.__receptor = receptor
         self.__message = self.__format_information(information)
     
-    def __format_information(self, information: any) -> any:
-        ''' header, body, token.. '''
-        return information
-    
-    def execute(self) -> None:
-        self.__receptor.process_information(self.__message)
+    def show_allpacientes(self):
+        return paciente.show_allpacientes()
